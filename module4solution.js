f135116fa70f72ea8c448f0e37ede8c7cd1d14bd
ertugrulsarihan <ertugrulsarihan1@gmail.com>
2020-12-15 22:17:57 +0300

    module4

---
+++ module4solution.js
@@ -0,0 +1,11 @@
+(function() {
+    var names = ["Ertugrul", "John", "Jordan", "Jason", "Paul", "Frank", "Lebron", "Salih", "Laura", "Jim"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
+})();
